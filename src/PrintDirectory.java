46cdab38c6122f7b549b61678373320b4edba0a8
pakubieniec <pakubieniec@gmail.com>
2023-10-17 22:46:21 +0200

    Update

--- src/PrintDirectory.java
+++ src/PrintDirectory.java
@@ -9,24 +9,6 @@ public class PrintDirectory {
     public static void main(String args[]) {
 
         printDirectory(new File("C:\\Users\\Patty\\Documents\\NetBeansProjects\\Mod-5"));
-
-            //Instantiating the Scanner class
-//            sc= new Scanner(file);
-//            String input;
-//            while (sc.hasNextLine()) {
-//                input = sc.nextLine();
-//                sb.append(input+" ");
-//            }
-//            System.out.println("Contents of the file: "+sb.toString());
-//            System.out.println(" ");
-//            //Instantiating the FileOutputStream class
-//            FileOutputStream fileOut = new FileOutputStream("E:\\Angielski Pawlikowska");
-//            //Instantiating the DataOutputStream class
-//            DataOutputStream outputStream = new DataOutputStream(fileOut);
-//            //Writing UTF data to the output stream
-//            outputStream.write(sb.toString().getBytes());
-//            outputStream.flush();
-//            System.out.println("Data entered into the file");
         }
 
     public static void printDirectory(File directoryPath) {
